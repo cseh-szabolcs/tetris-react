3c72a408bc198c128f17caa8091c4c8d9fdebed8
Cseh Szabolcs <szabolcs.cseh@gmail.com>
2018-05-16 01:15:32 +0200

    some funny coding-style

--- app/components/MultiPlayer.jsx
+++ app/components/MultiPlayer.jsx
@@ -1,8 +1,8 @@
+
 import React from 'react';
 import { connect } from 'react-redux';
 
 import MultiPlayerComponents from './MultiPlayer/index';
-
 const { Auth, UserItem } = MultiPlayerComponents;
 
 
