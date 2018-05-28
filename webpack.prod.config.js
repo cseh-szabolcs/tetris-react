a1e67b19b53c9cc773ee8b711e28e33d2395cbe0
Cseh Szabolcs <szabolcs.cseh@gmail.com>
2018-05-28 03:25:40 +0200

    fix login/game-running

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,9 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
 
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
-// entry: ['babel-polyfill'] -> must also be installed by npm with dev-dept
-
 
 module.exports = {
   // ================================================================
@@ -51,30 +48,23 @@ module.exports = {
       // SCSS ----
       {
         test: /\.scss$/,
-        //use: ExtractTextPlugin.extract({
-        //  fallback: "style-loader",
-          use: [
-            {loader: "style-loader"}, //<--
-            {
-              loader: "css-loader",
-              //options: { // ! occurs an warning !
-              //  minimize: true
-              //}
-            },
-            {
-              loader: "sass-loader",
-              options: {
-                includePaths: [path.resolve(__dirname, './node_modules/foundation-sites/scss')]
-              }
+        use: [
+          {loader: "style-loader"}, //<--
+          {
+            loader: "css-loader",
+           },
+          {
+            loader: "sass-loader",
+            options: {
+              includePaths: [path.resolve(__dirname, './node_modules/foundation-sites/scss')]
             }
-          ]
-        //})
+          }
+        ]
       }
     ]
   },
   // ================================================================
   plugins: [
-    // new ExtractTextPlugin("public/styles.css"), // [contenthash]
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
