247efdaee9409c28f6c562b8c556c9cf3ee5170c
Cseh Szabolcs <szabolcs.cseh@gmail.com>
2018-05-28 02:28:32 +0200

    config webpack

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -11,6 +11,9 @@ export const Welcome = ({ initGame }) => (
     <button className="button large expanded" onClick={ () => initGame() }>
       Start single-player!
     </button>
+    <a href="https://github.com/cseh-szabolcs/tetris-react" target="_blank">
+      Source on GitHub &raquo;
+    </a>
   </div>
 );
 
