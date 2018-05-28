88d0264b9ad85b84a0312af10b0a6f85246c6fae
Cseh Szabolcs <szabolcs.cseh@gmail.com>
2018-05-28 03:24:40 +0200

    fix login/game-running

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -74,13 +74,18 @@ module.exports = {
   },
   // ================================================================
   plugins: [
-    /*
-    new ExtractTextPlugin("public/styles.css"), // [contenthash]
+    // new ExtractTextPlugin("public/styles.css"), // [contenthash]
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: false
+      sourceMap: true,   // enable source maps to map errors (stack traces) to modules
+      output: {
+        comments: false, // remove all comments
+      },
     })
-    */
+
+
   ],
   // ================================================================
   devtool: 'cheap-module-eval-source-map'
