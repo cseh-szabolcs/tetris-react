d0e8cd0435e2915265cf2be2ed60491c96f8de1f
Cseh Szabolcs <szabolcs.cseh@gmail.com>
2018-06-02 16:01:03 +0200

    fix tab master-slave-bug by adding an ping/pong-logic between them

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,12 +5,14 @@ import { connect } from 'react-redux';
 import actions from 'tetris-actions';
 
 
-export const Welcome = ({ initGame }) => (
+export const Welcome = ({ initGame, isMasterTab = true }) => (
   <div className="tetris-welcome">
     <h1>Tetris</h1>
-    <button className="button large expanded" onClick={ () => initGame() }>
-      Start single-player!
-    </button>
+    { isMasterTab && (
+      <button className="button large expanded" onClick={ () => initGame() }>
+        Start single-player!
+      </button>
+    )}
     <a href="https://github.com/cseh-szabolcs/tetris-react" target="_blank">
       Source on GitHub &raquo;
     </a>
@@ -18,7 +20,10 @@ export const Welcome = ({ initGame }) => (
 );
 
 
-export default connect(null,
+export default connect(
+  (state) => ({
+    isMasterTab: state.window.masterTab,
+  }),
   (dispatch) => ({
     initGame: () => dispatch(actions.game.init({})),
   }),
